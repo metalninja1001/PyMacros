fb11f4b1dd35de0c32b4e3a5d0a658580b64cdec
metalninja1001 <101802030+metalninja1001@users.noreply.github.com>
2022-08-16 15:15:31 +0200

    Update getmouse.py

--- getmouse.py
+++ getmouse.py
@@ -3,9 +3,3 @@ import pyautogui
 
 time.sleep(10)
 print(pyautogui.position())
-#pyautogui.click(button='right', x=12, y=1061)
-#pyautogui.click(x=69, y=963)
-#pyautogui.click(x=158, y=994)
-
-#pyautogui.write('cd Desktop')
-#pyautogui.press('enter')
