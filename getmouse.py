ef0d7ad183395c19cadeb8873508c14e1effa824
metalninja1001 <101802030+metalninja1001@users.noreply.github.com>
2022-08-16 14:57:26 +0200

    Add files via upload

---
+++ getmouse.py
@@ -0,0 +1,11 @@
+import time
+import pyautogui
+
+time.sleep(10)
+print(pyautogui.position())
+#pyautogui.click(button='right', x=12, y=1061)
+#pyautogui.click(x=69, y=963)
+#pyautogui.click(x=158, y=994)
+
+#pyautogui.write('cd Desktop')
+#pyautogui.press('enter')
