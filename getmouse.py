288f600ce8459be6f23035442f379712c0862c96
metalninja1001 <101802030+metalninja1001@users.noreply.github.com>
2022-08-17 19:01:46 +0200

    Update getmouse.py

--- getmouse.py
+++ getmouse.py
@@ -1,5 +1,8 @@
+#import modules
 import time
 import pyautogui
 
+#set delay between when the script is executed until the mouse location is recorded.
 time.sleep(10)
+#print mouse location with x and y coordinates.
 print(pyautogui.position())
