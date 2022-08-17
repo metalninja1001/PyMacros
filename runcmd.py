1cb87b501ab03a6c238aea5e438fff104ee490a8
metalninja1001 <101802030+metalninja1001@users.noreply.github.com>
2022-08-17 19:03:52 +0200

    Update runcmd.py

--- runcmd.py
+++ runcmd.py
@@ -1,6 +1,8 @@
+#import modules
 import time
 import pyautogui
 
+#configure recorded macros with click function.
 pyautogui.click(button='right', x=12, y=1061)
 pyautogui.click(x=69, y=963)
 pyautogui.click(x=158, y=994)
